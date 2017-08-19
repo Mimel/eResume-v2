124a6a7badc1775a455df92ecaf82bf4436d3f31
Matt Imel <imel.matt.r@gmail.com>
2017-08-19 18:37:52 -0500

    Added basic sidebar formatting, functionality. Selected project now highlights on click.

--- src/app/project-list.component.ts
+++ src/app/project-list.component.ts
@@ -9,8 +9,34 @@ import { ProjectRetrieval } from './project.retrieval.service';
 })
 export class ProjListComponent {
   titles = [];
+  selectedTitle;
+
   constructor(private retrievalService : ProjectRetrieval) {
-    retrievalService.getProjectList().subscribe((resp) => {this.titles = resp},
-                                                (error) => {});
+    retrievalService.getProjectList().subscribe((resp) => {
+      var i = 0;
+      resp.forEach(element => this.titles.push(new TitleElement(element.username, i++)))
+      this.selectedTitle = this.titles[0];
+    },
+    (error) => {});
+  }
+
+  selectTitle(titleNo: number) {
+    if(titleNo < this.titles.length && titleNo >= 0) {
+      this.selectedTitle = this.titles[titleNo];
+    }
+  }
+
+  isSelectedTitle(titleNo: number) {
+    return (titleNo === this.selectedTitle.number);
+  }
+}
+
+class TitleElement {
+  name;
+  number;
+
+  constructor(name: string, no: number) {
+    this.name = name;
+    this.number = no;
   }
 }
