c606774b5a68c95c645ccb9d79aac64457df1046
Matt Imel <imel.matt.r@gmail.com>
2017-10-04 09:31:56 -0500

    Added sliding functionality to active bar in sidebar.

--- src/app/project-list.component.ts
+++ src/app/project-list.component.ts
@@ -8,13 +8,23 @@ import * as $ from 'jquery';
   styleUrls: ['project_list_styles.css'],
   templateUrl: 'project_list.html'
 })
+
 export class ProjListComponent {
 
   constructor(private dataService : ProjectData) {}
 
+  getOffset() {
+    //Accounts for both the height of the title and the separators.
+    return this.dataService.active * 55;
+  }
+
   ngAfterViewInit() {
-    $( "#pl_complete_list" ).on("click", ".pl_wrapper", function() {
-      $(this).css("color", "red");
-    });
+    $("#pl_complete_list").on("click", ".pl_wrapper", {env: this}, shiftBar);
+
+    function shiftBar(event) {
+      $("#pl_slider").animate({
+        top: event.data.env.getOffset()
+      });
+    }
   }
 }
