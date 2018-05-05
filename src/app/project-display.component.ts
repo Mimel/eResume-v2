df4e52c4ff84d591f79c61299ca8b5781350c014
Mimel <imel.matt.r@gmail.com>
2018-05-05 03:11:26 -0500

    Added primordial implementation of PhotoSwipe.

--- src/app/project-display.component.ts
+++ src/app/project-display.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ProjectData } from './project.retrieval.service';
+import * as $ from 'jquery';
+import PhotoSwipe from 'photoswipe';
+import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default';
 
 @Component({
   selector: 'project-display',
@@ -9,4 +12,40 @@ import { ProjectData } from './project.retrieval.service';
 })
 export class ProjDisplayComponent {
   constructor(private dataService: ProjectData) {}
+
+  ngAfterViewInit() {
+
+      $("#display_wrapper").on("click", ".display_thumbnail", {env: this}, showGallery);
+
+      function showGallery(event) {
+          var pswpElement = document.querySelectorAll('.pswp')[0];
+
+          console.log("Hello!");
+
+          // build items array
+          var items = [
+              {
+                  src: 'https://placekitten.com/600/400',
+                  w: 600,
+                  h: 400
+              },
+              {
+                  src: 'https://placekitten.com/1200/900',
+                  w: 1200,
+                  h: 900
+              }
+          ];
+
+          // define options (if needed)
+          var options = {
+              // optionName: 'option value'
+              // for example:
+              index: 0 // start at first slide
+          };
+
+          // Initializes and opens PhotoSwipe
+          var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+          gallery.init();
+    }
+  }
 }
